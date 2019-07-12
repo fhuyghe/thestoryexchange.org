7a0a25f13095a9fe0ca6a3ca00812f0417b2ae68
Francois Huyghe <francois@13milliseconds.com>
2019-07-12 14:38:22 -0500

    Added server caching to Nginx

--- site/config/environments/development.php
+++ site/config/environments/development.php
@@ -6,7 +6,7 @@
 use Roots\WPConfig\Config;
 
 Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG', false);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
 
