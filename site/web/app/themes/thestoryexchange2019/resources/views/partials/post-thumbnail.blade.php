c7fd73a72f53587f0597a1c7121f8045eb3d41a7
Francois Huyghe <francois@13milliseconds.com>
2019-08-06 16:34:56 -0500

    updated name of simple-podcast-press

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/post-thumbnail.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/post-thumbnail.blade.php
@@ -1,7 +1,7 @@
 <div class="thumbnail">
-    <div class="fill">
-        <a href="{!! the_permalink() !!}">
+    <a href="{!! the_permalink() !!}">
+        <div class="fill">
             {!! the_post_thumbnail('medium') !!}
-        </a>
-    </div>
+        </div>
+    </a>
 </div>
