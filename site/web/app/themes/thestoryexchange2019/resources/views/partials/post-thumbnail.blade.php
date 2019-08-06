6704637b02ccd5a8698f712843a6a90c846d781a
Cory Hall <corydantehall@gmail.com>
2019-08-06 18:57:02 -0400

    more responsive changes

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/post-thumbnail.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/post-thumbnail.blade.php
@@ -1,6 +1,6 @@
 <div class="thumbnail">
     <div class="fill">
-        <a href="{!! the_permalink() !!}">
+        <a href="{!! the_permalink() !!}" style="background-image: url('{!! get_the_post_thumbnail_url(null, 'medium') !!}')" class="post-image-link">
             {!! the_post_thumbnail('medium') !!}
         </a>
     </div>
