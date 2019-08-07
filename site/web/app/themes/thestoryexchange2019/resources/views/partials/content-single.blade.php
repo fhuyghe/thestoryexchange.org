cb7e87e5db056edab3fe5ce3c5954563beef8999
Francois Huyghe <francois@13milliseconds.com>
2019-08-07 12:11:02 -0500

    Simple post formatting

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/content-single.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/content-single.blade.php
@@ -1,8 +1,19 @@
-<article @php post_class() @endphp>
-  <header>
+<article {!! post_class() !!}>
+  <header class="page-header">
     <h1 class="entry-title">{!! get_the_title() !!}</h1>
+    <p class="subtitle"><?php the_subheading(); ?></p>
     @include('partials/entry-meta')
   </header>
+
+  @if ( in_category( 3 ))
+    <section class="video">
+        <div class="iframewrap">
+            <?php the_field('video'); ?>
+        </div>
+        <p class="caption"><?php the_field('video_caption'); ?></p>
+    </section>
+  @endif
+
   <div class="entry-content">
     @php the_content() @endphp
   </div>
