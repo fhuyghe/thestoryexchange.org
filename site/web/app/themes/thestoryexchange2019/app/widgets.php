c0321306f0f7f2e14fcc1f9b213c2ab8bbdb0194
Francois Huyghe <francois@13milliseconds.com>
2019-08-08 17:04:31 -0500

    Podcast and Video widget structure

--- site/web/app/themes/thestoryexchange2019/app/widgets.php
+++ site/web/app/themes/thestoryexchange2019/app/widgets.php
@@ -32,11 +32,67 @@ function __construct() {
     echo $args['before_widget'];
     if ( ! empty( $title ) )
     echo $args['before_title'] . $title . $args['after_title'];
-    
-    // This is where you run the code and display the output
-    echo __( 'Hello, World!', 'widget_video_domain' );
+
+            $query_args=array(
+              'post_type' => 'post',
+              'post_status' => 'publish',
+              'posts_per_page' => 1,
+              'category_name' => 'entrepreneur-videos'
+            );
+            $video_query = new WP_Query( $query_args );
+                if ( $video_query -> have_posts() ) : while ( $video_query -> have_posts() ) : $video_query ->the_post(); ?>
+              <div class="download-box video-post">
+                  <div class="thumbnail">
+                    <div class="wrap-out">
+                  <img width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
+                  <div class="wrap-in">
+                        <div class="play-button"><i class="fas fa-play"></i></div>
+                      </div>
+                      </div>
+                      </div>
+                <div class="blurb">
+                      <a href="<?php the_permalink(); ?>"><h6><?php the_title(); ?></h6></a>
+                      <?php the_excerpt(); ?>
+                </div>
+                <div class="video-player">
+                    <div class="wrap">
+                    <div class="embed-container">
+                    <?php
+                            $iframe = get_field('video');
+                            // use preg_match to find iframe src
+                            if ($iframe):
+                              preg_match('/src="(.+?)"/', $iframe, $matches);
+                              preg_match("/src='(.+?)'/", $iframe, $othermatches);
+                              $src = $matches[1];
+                              if (!$src) {
+                                $src = $othermatches[1];
+                              }
+                              echo "<script>console.log( 'Debug Objects: " . $src . "' );</script>";
+                              $cleanedsrc = explode('?', $src);
+                              // add extra params to iframe src
+                              $new_src = $cleanedsrc[0] . '?autoplay=1';
+                              echo "<script>console.log( 'Debug Objects: " . $new_src . "' );</script>";
+                              $iframe = str_replace($src, $new_src, $iframe);
+                              $iframe = str_replace('og-src', 'src', $iframe);
+                              // add extra attributes to iframe html
+                              $attributes = 'frameborder="0"';
+                              $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+                              // echo $iframe
+                              echo $iframe;
+                            ?>
+                          <?php endif; ?>
+                      </div>
+                      <div class="close"><i class="far fa-times"></i></div>
+                      </div>
+                  </div>
+              </div>
+           <?php endwhile; endif; ?>
+           <a class="bluebutton" href="<?php home_url(); ?>/category/entrepreneur-videos/">More videos</a>
+
+    <?php
     echo $args['after_widget'];
     }
+
          
     // Widget Backend 
     public function form( $instance ) {
@@ -65,20 +121,14 @@ function __construct() {
 
 // Creating the Video widget 
 class widget_podcast extends WP_Widget {
- 
-function __construct() {
-    parent::__construct(
     
-    // Base ID of your widget
-    'widget_podcast', 
-    
-    // Widget name will appear in UI
-    __('Latest TSE Podcast Episode', 'widget_podcast_domain'), 
-    
-    // Widget description
-    array( 'description' => __( 'Displays the latest video and launches a player.', 'widget_podcast_domain' ), )
-    );
-}
+    function __construct() {
+        parent::__construct(
+            'widget_podcast', 
+            __('Latest TSE Podcast Episode', 'widget_podcast_domain'), 
+            array( 'description' => __( 'Displays the latest video and launches a player.', 'widget_podcast_domain' ), )
+        );
+    }
  
     // Creating widget front-end
     public function widget( $args, $instance ) {
@@ -99,7 +149,7 @@ function __construct() {
         $my_query = new WP_Query( $query_args );
             if ( $my_query -> have_posts() ) : while ( $my_query -> have_posts() ) : $my_query ->the_post(); ?>
            <div class="download-box">
-               <img style="margin-top: 0px !important; margin-bottom:0px !important; " width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
+               <img width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
          	<div  class="blurb">
                 <?php echo do_shortcode('[spp-player]'); ?>
                 <a href="<?php the_permalink(); ?>"><h6><?php the_title(); ?></h6></a>
@@ -118,6 +168,7 @@ function __construct() {
     echo $args['after_widget'];
     }
          
+
     // Widget Backend 
     public function form( $instance ) {
     if ( isset( $instance[ 'title' ] ) ) {
@@ -128,10 +179,10 @@ function __construct() {
     }
     // Widget admin form
     ?>
-    <p>
-    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-    </p>
+        <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
     <?php 
     }
      
