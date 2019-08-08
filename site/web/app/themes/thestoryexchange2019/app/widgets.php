880906845900f5e046ada378b1ba9937c0f3f316
Francois Huyghe <francois@13milliseconds.com>
2019-08-08 17:29:52 -0500

    Better styling on podcast and video widgets

--- site/web/app/themes/thestoryexchange2019/app/widgets.php
+++ site/web/app/themes/thestoryexchange2019/app/widgets.php
@@ -10,19 +10,13 @@ add_action( 'widgets_init', 'wp_load_widget' );
 // Creating the Video widget 
 class widget_video extends WP_Widget {
  
-function __construct() {
-    parent::__construct(
-    
-    // Base ID of your widget
-    'widget_video', 
-    
-    // Widget name will appear in UI
-    __('Latest TSE Video Episode', 'widget_video_domain'), 
-    
-    // Widget description
-    array( 'description' => __( 'Displays the latest video and launches a player.', 'widget_video_domain' ), )
-    );
-}
+    function __construct() {
+        parent::__construct(
+        'widget_video', 
+        __('Latest TSE Video Episode', 'widget_video_domain'), 
+        array( 'description' => __( 'Displays the latest video and launches a player.', 'widget_video_domain' ), )
+        );
+    }
  
     // Creating widget front-end
     public function widget( $args, $instance ) {
@@ -33,61 +27,63 @@ function __construct() {
     if ( ! empty( $title ) )
     echo $args['before_title'] . $title . $args['after_title'];
 
-            $query_args=array(
-              'post_type' => 'post',
-              'post_status' => 'publish',
-              'posts_per_page' => 1,
-              'category_name' => 'entrepreneur-videos'
-            );
-            $video_query = new WP_Query( $query_args );
-                if ( $video_query -> have_posts() ) : while ( $video_query -> have_posts() ) : $video_query ->the_post(); ?>
-              <div class="download-box video-post">
-                  <div class="thumbnail">
-                    <div class="wrap-out">
-                  <img width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
-                  <div class="wrap-in">
+        $query_args=array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'posts_per_page' => 1,
+            'category_name' => 'entrepreneur-videos'
+        );
+        $video_query = new WP_Query( $query_args );
+            if ( $video_query -> have_posts() ) : while ( $video_query -> have_posts() ) : $video_query ->the_post(); ?>
+            <div class="download-box video-post">
+                <div class="thumbnail">
+                <div class="wrap-out">
+                    <div class="fill">
+                        <img width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
+                    </div>
+                    <div class="wrap-in">
                         <div class="play-button"><i class="fas fa-play"></i></div>
-                      </div>
-                      </div>
-                      </div>
-                <div class="blurb">
-                      <a href="<?php the_permalink(); ?>"><h6><?php the_title(); ?></h6></a>
-                      <?php the_excerpt(); ?>
+                    </div>
                 </div>
-                <div class="video-player">
-                    <div class="wrap">
-                    <div class="embed-container">
-                    <?php
-                            $iframe = get_field('video');
-                            // use preg_match to find iframe src
-                            if ($iframe):
-                              preg_match('/src="(.+?)"/', $iframe, $matches);
-                              preg_match("/src='(.+?)'/", $iframe, $othermatches);
-                              $src = $matches[1];
-                              if (!$src) {
-                                $src = $othermatches[1];
-                              }
-                              echo "<script>console.log( 'Debug Objects: " . $src . "' );</script>";
-                              $cleanedsrc = explode('?', $src);
-                              // add extra params to iframe src
-                              $new_src = $cleanedsrc[0] . '?autoplay=1';
-                              echo "<script>console.log( 'Debug Objects: " . $new_src . "' );</script>";
-                              $iframe = str_replace($src, $new_src, $iframe);
-                              $iframe = str_replace('og-src', 'src', $iframe);
-                              // add extra attributes to iframe html
-                              $attributes = 'frameborder="0"';
-                              $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
-                              // echo $iframe
-                              echo $iframe;
-                            ?>
-                          <?php endif; ?>
-                      </div>
-                      <div class="close"><i class="far fa-times"></i></div>
-                      </div>
-                  </div>
-              </div>
-           <?php endwhile; endif; ?>
-           <a class="bluebutton" href="<?php home_url(); ?>/category/entrepreneur-videos/">More videos</a>
+            </div>
+            <div class="blurb">
+                    <a href="<?php the_permalink(); ?>"><h6><?php the_title(); ?></h6></a>
+                    <?php the_excerpt(); ?>
+            </div>
+            <div class="video-player">
+                <div class="wrap">
+                <div class="embed-container">
+                <?php
+                        $iframe = get_field('video');
+                        // use preg_match to find iframe src
+                        if ($iframe):
+                            preg_match('/src="(.+?)"/', $iframe, $matches);
+                            preg_match("/src='(.+?)'/", $iframe, $othermatches);
+                            $src = $matches[1];
+                            if (!$src) {
+                            $src = $othermatches[1];
+                            }
+                            echo "<script>console.log( 'Debug Objects: " . $src . "' );</script>";
+                            $cleanedsrc = explode('?', $src);
+                            // add extra params to iframe src
+                            $new_src = $cleanedsrc[0] . '?autoplay=1';
+                            echo "<script>console.log( 'Debug Objects: " . $new_src . "' );</script>";
+                            $iframe = str_replace($src, $new_src, $iframe);
+                            $iframe = str_replace('og-src', 'src', $iframe);
+                            // add extra attributes to iframe html
+                            $attributes = 'frameborder="0"';
+                            $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+                            // echo $iframe
+                            echo $iframe;
+                        ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="close"><i class="far fa-times"></i></div>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; endif; ?>
+        <a class="bluebutton" href="<?php home_url(); ?>/category/entrepreneur-videos/">More videos</a>
 
     <?php
     echo $args['after_widget'];
@@ -148,19 +144,24 @@ class widget_podcast extends WP_Widget {
         );
         $my_query = new WP_Query( $query_args );
             if ( $my_query -> have_posts() ) : while ( $my_query -> have_posts() ) : $my_query ->the_post(); ?>
-           <div class="download-box">
-               <img width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
-         	<div  class="blurb">
-                <?php echo do_shortcode('[spp-player]'); ?>
-                <a href="<?php the_permalink(); ?>"><h6><?php the_title(); ?></h6></a>
-                <?php the_excerpt(); ?>
-		  </div>
-          <!-- REPLACE WITH PODCAST IMAGE-->
+                <div class="download-box">
+                <div class="thumbnail">
+                    <div class="fill">
+                        <img width="100%" height="auto" src=" <?php the_post_thumbnail_url('medium'); ?>" >
+                    </div>
+                </div>
+
+                <div  class="blurb">
+                    <?php echo do_shortcode('[spp-player]'); ?>
+                    <a href="<?php the_permalink(); ?>"><h6><?php the_title(); ?></h6></a>
+                    <?php the_excerpt(); ?>
+                </div>
+
                 <div class="sppbuttons bottom">
-               <a class="button-itunes" target="_blank" style="" href="https://itunes.apple.com/us/podcast/the-story-exchange/id1036000689?mt=2">iTunes</a>
-				<a class="button-stitcher" target="_blank" style="" href="http://www.stitcher.com/s?fid=72717&refid=stpr">Stitcher</a>
+                    <a class="button-itunes" target="_blank" style="" href="https://itunes.apple.com/us/podcast/the-story-exchange/id1036000689?mt=2">iTunes</a>
+				    <a class="button-stitcher" target="_blank" style="" href="http://www.stitcher.com/s?fid=72717&refid=stpr">Stitcher</a>
                </div>
-           </div>
+               </div>
            <?php endwhile; endif; ?>
            <a class="bluebutton" href="/podcast">More Episodes</a>
     
