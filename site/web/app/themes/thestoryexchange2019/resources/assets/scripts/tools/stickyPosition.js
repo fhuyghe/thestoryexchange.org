bce47ddc28c70b5384937148af6b5520850f870e
Cory Hall <corydantehall@gmail.com>
2019-08-09 00:40:45 -0400

    handle resize appropriately

--- site/web/app/themes/thestoryexchange2019/resources/assets/scripts/tools/stickyPosition.js
+++ site/web/app/themes/thestoryexchange2019/resources/assets/scripts/tools/stickyPosition.js
@@ -21,21 +21,24 @@ export default function stickyPosition(selectors, eventTarget = window) {
 
     const insertPlaceholder = ([p, el]) => el.insertAdjacentElement('afterEnd', p);
     const removePlaceholder = ([p]) => p.remove()
+    const elementHandler = ([placeholder, el]) => {
+      const elHasClass = el.classList.contains(stickyClassBase);
+      const elRect = el.getBoundingClientRect();
+      const {y, height} = elHasClass ? placeholder.getBoundingClientRect() : elRect;
+
+      if (elHasClass && y >= 0) {
+          el.classList.remove(stickyClassBase);
+          placeholder.style.height = 0;
+      } else if (!elHasClass && y < 0) {
+        el.classList.add(stickyClassBase);
+        placeholder.style.height = `${height}px`;
+      } else if (elRect.height !== height) {
+        placeholder.style.height = `${elRect.height}px`;
+      }
+    };
 
     const updater = () => {
-      placeholderElements.forEach(([placeholder, el]) => {
-        const elHasClass = el.classList.contains(stickyClassBase);
-        const {y, height} = (elHasClass ? placeholder : el).getBoundingClientRect();
-
-        if (elHasClass && y > 0) {
-            el.classList.remove(stickyClassBase);
-            placeholder.style.height = 0;
-        } else if (!elHasClass && y <= 0) {
-          el.classList.add(stickyClassBase);
-          placeholder.style.height = `${height}px`;
-        }
-      })
-      // tell elements positioning
+      placeholderElements.forEach(elementHandler)
     };
 
     const [install, uninstall] = loader(throttle(updater, 50));
