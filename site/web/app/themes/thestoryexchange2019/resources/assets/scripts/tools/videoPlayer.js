e632f8306dc5b1f898258f3be2884b77fe5f20f0
Cory Hall <corydantehall@gmail.com>
2019-08-08 21:16:08 -0400

    extract video player initialization

---
+++ site/web/app/themes/thestoryexchange2019/resources/assets/scripts/tools/videoPlayer.js
@@ -0,0 +1,30 @@
+// #######################################
+// VIDEO PLAYERS #########################
+// #######################################
+
+export default function initVideoPlayers() {
+  if ( $('.video-player').length){
+
+    $('.video-post iframe, .playlist iframe').each(function() {
+        $(this).attr('og-src', $(this).attr('src'));
+        $(this).attr('src', '');
+    });
+
+    $('.video-post .thumbnail, .playlist .thumbnail').click(function(){
+        $('.video-player', $(this).parent()).css('display', 'flex');
+        var iframe = $('iframe', $(this).parent());
+        var source = iframe.attr('src');
+        if (iframe.attr('og-src')) {
+            source = iframe.attr('og-src');
+        }
+        iframe.attr('src', source);
+    });
+
+    $('.video-player').click(function(){
+        var iframe = $('iframe', this);
+        iframe.attr('og-src', iframe.attr('src'));
+        iframe.attr('src', '');
+        $(this).hide();
+    });
+  }
+}
