818edfd9a3deba16046b8cbc94466cf39cce0ad7
Cory Hall <corydantehall@gmail.com>
2019-08-10 20:59:33 -0400

    use icons for button

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/side-menu.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/side-menu.blade.php
@@ -2,6 +2,8 @@
 {{ $slot }}
 </div>
 <button class="side-menu__toggle-button" href="#">
+  <i class="fas fa-bars side-menu__toggle-button__icon--open"></i>
+  <i class="fas fa-times side-menu__toggle-button__icon--close"></i>
 </button>
 <div class="side-menu__container" style="visibility: hidden">
   <h2 class="site-name">
