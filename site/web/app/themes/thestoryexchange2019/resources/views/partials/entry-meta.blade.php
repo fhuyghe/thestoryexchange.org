b2d346103b252e35f67cb53c347efa30ea605358
Francois Huyghe <francois@13milliseconds.com>
2019-08-26 13:19:41 -0500

    Fixing user avatars

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/entry-meta.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/entry-meta.blade.php
@@ -1,8 +1,8 @@
 <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
 <div class="author-meta">
     <div class="author_thumbnail">
-      <img src="<?php echo esc_url( get_avatar_url( get_the_author_meta('ID') ) ); ?>" />
+      {!! userphoto_the_author_thumbnail($post_author) !!}
     </div>
-    <?php _e( 'By ', 'continuum' ); ?>
-    <?php the_author_link(); ?>
+    {!! _e( 'By ', 'continuum' ) !!}
+    {!! the_author_link() !!}
 </div>
