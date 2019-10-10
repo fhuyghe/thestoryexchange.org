a5433d939d67a3c3d163803ead1fe66f801d5426
Francois Huyghe <francois@13milliseconds.com>
2019-10-10 11:26:34 -0500

    Section layout on podcast page

--- site/web/app/themes/thestoryexchange2019/resources/views/template-podcast.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/template-podcast.blade.php
@@ -10,19 +10,20 @@
     @while(have_posts()) @php the_post() @endphp
       <h2>{{ the_title() }}</h2>
       <?php the_content(); ?>
-      <div class="buttonsWrap">
+      <section class="buttonsWrap">
+        {{ the_field('sharing_text') }}
         <a class="button-itunes" target="_blank" style="" href="https://itunes.apple.com/us/podcast/the-story-exchange/id1036000689?mt=2">iTunes</a>
         <a class="button-stitcher" target="_blank" style="" href="http://www.stitcher.com/s?fid=72717&refid=stpr">Stitcher</a>
         <a class="button-google spp-button-custom1" target="_blank" href="https://www.google.com/podcasts?feed=aHR0cDovL3RoZXN0b3J5ZXhjaGFuZ2UubGlic3luLmNvbS9yc3M%3D">Google</a>
         <a class="button-spprss" target="_blank" href="http://thestoryexchange.libsyn.com/rss">RSS</a>	
-      </div>
-      <div class="share">
+      </section>
+      <section class="share">
         {{ the_field('share_your_story_text') }}
         @php $link = get_field('share_your_story_link') @endphp
         @if ($link)
           <a class="bigbutton yellow" target="_blank" href="{{ $link }}">Share Your Story</a>
         @endif
-      </div>
+      </section>
       @endwhile
   </div>
 
