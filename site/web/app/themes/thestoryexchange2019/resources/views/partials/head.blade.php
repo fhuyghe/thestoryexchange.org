f3bf69c7bb30310b7056fc64b7b077809eaf5c39
Francois Huyghe <francois@13milliseconds.com>
2019-10-19 15:11:07 -0500

    Moved the Tag Manager again

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/head.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/head.blade.php
@@ -14,4 +14,12 @@
 
   {{-- FONT AWESOME --}}
   <script src="https://kit.fontawesome.com/800e21cc29.js"></script>
+
+  <!-- Google Tag Manager begins -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-T492J8');</script>
+  <!-- End Google Tag Manager -->
 </head>
