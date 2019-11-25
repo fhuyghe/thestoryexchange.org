d7daef5336dd1d660363844acfa3225e71480e20
Francois Huyghe <francois@13milliseconds.com>
2019-11-25 15:10:32 -0600

    Added a numbered list styling

--- site/web/app/themes/thestoryexchange2019/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/thestoryexchange2019/resources/assets/scripts/routes/common.js
@@ -6,6 +6,20 @@ import collapsibleMenu from '../tools/collapsibleMenu';
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    //Make all outbound links open in a new tab
+    $('a').each(function () {
+        console.log('Checking links')
+        var a = new RegExp('/' + window.location.host + '/');
+        if (!a.test(this.href)) {
+          console.log(this);
+          $(this).click(function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            window.open(this.href, '_blank');
+          });
+        }
+      });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
