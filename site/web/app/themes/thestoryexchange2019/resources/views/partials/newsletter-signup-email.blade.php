3c17e921f38bae7cc7ab4e8fd044ef6510c075bd
Francois Huyghe <francois@13milliseconds.com>
2020-01-09 14:46:43 -0600

    Newsletter signup on banner

--- site/web/app/themes/thestoryexchange2019/resources/views/partials/newsletter-signup-email.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/partials/newsletter-signup-email.blade.php
@@ -1,5 +1,5 @@
 <!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+{{-- <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
 <div id="mc_embed_signup">
 <form action="https://thestoryexchange.us2.list-manage.com/subscribe/post?u=b446574c4c4c1d75475aa8b75&amp;id=00e4d55029" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
@@ -10,6 +10,10 @@
     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
 </form>
-</div>
+</div> --}}
 
 <!--End mc_embed_signup-->
+
+ <!--JB Tracker--> <script type="text/javascript"> var _paq = _paq || []; (function(){ if(window.apScriptInserted) return; _paq.push(['clientToken', 'P%2bsIjEMd6oQ%3d']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://prod.benchmarkemail.com/tracker.bundle.js'; s.parentNode.insertBefore(g,s); window.apScriptInserted=true;})(); </script> <!--/JB Tracker--> <!-- BEGIN: Benchmark Email Signup Form Code -->
+ <script type="text/javascript" id="lbscript1299648" src="https://lb.benchmarkemail.com//code/lbformnew.js?mFcQnoBFKMSecPfRIkLLIFgg49YEcrgGWS4PGTYbqIA73fD7Zl3axA%253D%253D"></script><noscript>Please enable JavaScript <br /></noscript>
+ <!-- END: Benchmark Email Signup Form Code -->
