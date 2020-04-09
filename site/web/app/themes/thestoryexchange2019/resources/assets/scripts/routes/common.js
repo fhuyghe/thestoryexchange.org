03010b772d72cb716040c66594dd8dbb97f5fd95
Francois Huyghe <francois@13milliseconds.com>
2020-04-09 17:04:03 -0500

    All links are outbound

--- site/web/app/themes/thestoryexchange2019/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/thestoryexchange2019/resources/assets/scripts/routes/common.js
@@ -8,16 +8,21 @@ export default {
     // JavaScript to be fired on all pages
 
     //Make all outbound links open in a new tab
-    $('article a').each(function () {
-        var a = new RegExp('/' + window.location.host + '/');
-        if (!a.test(this.href)) {
-          $(this).click(function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            window.open(this.href, '_blank');
-          });
-        }
-      });
+    // $('a').each(function () {
+    //     var a = new RegExp('/' + window.location.host + '/');
+    //     if (!a.test(this.href)) {
+    //       $(this).click(function(event) {
+    //         event.preventDefault();
+    //         event.stopPropagation();
+    //         window.open(this.href, '_blank');
+    //       });
+    //     }
+    // });
+    $('article a').click(function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      window.open(this.href, '_blank');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
