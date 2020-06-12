009717b7720caeb3733479be1bc4fb97e04232d2
Francois Huyghe <francois@13milliseconds.com>
2020-06-11 19:15:17 -0500

    Layout edits

--- site/web/app/themes/thestoryexchange2019/resources/views/page-100-years-of-power.blade.php
+++ site/web/app/themes/thestoryexchange2019/resources/views/page-100-years-of-power.blade.php
@@ -17,16 +17,15 @@
 	</section>
 
 	<section id="episodes" class="row">
-		<h3>Episodes</h3>
 		@if( have_rows('podcast_episodes') )
     	@while ( have_rows('podcast_episodes') ) @php the_row() @endphp
-		<div class="col-md-3">
+		<div class="col-md-3 episode">
 			<div class="thumbnail">
-				@php $image = the_sub_field('thumbnail') @endphp
-				<img src="{!! $image['url'] !!}" alt="{!! $image['alt'] !!}" />
+				@php $image = get_sub_field('thumbnail') @endphp
+				<img src="{!! $image['sizes']['medium'] !!}" alt="{!! $image['alt'] !!}" />
 			</div>
-			<h4>{{the_sub_field('title')}}</h4>
-			<p>{{the_sub_field('decription')}}</p>
+			<h4>{{ the_sub_field('title') }}</h4>
+			<p>{{ the_sub_field('description') }}</p>
 		</div>
     	@endwhile
 		@endif
