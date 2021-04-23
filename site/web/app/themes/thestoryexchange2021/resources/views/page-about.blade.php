f5e66b3b641fe6758449cc36134ab537840306a0
Francois Huyghe <francois@13milliseconds.com>
2021-04-23 17:35:00 -0400

    Updated About page

--- site/web/app/themes/thestoryexchange2021/resources/views/page-about.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/page-about.blade.php
@@ -12,36 +12,23 @@
     @php the_content() @endphp
     </section>
 
-    <section class="featured">
-        <h3>Featured in</h3>
-        <div class="features row">
-          @if( have_rows('as_featured_in') )
-          @while ( have_rows('as_featured_in') ) @php the_row() @endphp
-          <div class="feature col-md-3 col-6">
-            <a href="{{ the_sub_field('link') }}">
-                @php $image = get_sub_field('logo') @endphp
-                <img src="{{ $image['sizes']['partner-logo'] }}" alt="{!! $image['alt'] !!}" />
-              </a>
-            </div>
-            @endwhile
-          </div>
-      @endif
-    </section>
-
     <section class="who">
-      <h3>Who We Are</h3>
+      <h2>Who We Are</h2>
       @if( have_rows('who_we_are') )
+      <div class="row">
         @while ( have_rows('who_we_are') ) @php the_row() @endphp
-          <div class="about-person">
+          <div class="about-person col-md-6">
             <div class="about-photo">
-                @php $image = get_sub_field('photo') @endphp
-                <img src="{{ $image['url'] }}" alt="{!! $image['alt'] !!}" />
+              @php $image = get_sub_field('photo') @endphp
+              <img src="{{ $image['sizes']['medium'] }}" alt="{!! $image['alt'] !!}" />
             </div>
             <div class="about-text">
+              <h3>{!! the_sub_field('name') !!}</h3>
               {!! the_sub_field('bio') !!}
             </div>
           </div>
         @endwhile
+      </div>
       @endif
     </section>
   @endwhile
