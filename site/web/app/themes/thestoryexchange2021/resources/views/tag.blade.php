bfac9cca71e1d5a7053fb1f24d19037a7989486d
Francois Huyghe <francois@13milliseconds.com>
2021-05-11 11:13:19 -0400

    Tag page

---
+++ site/web/app/themes/thestoryexchange2021/resources/views/tag.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <section class="category-header">
+    @php $cat = get_queried_object() @endphp
+    <h1>{!! $cat->name !!}</h1>
+    <p>{!! $cat->description !!}</p>
+  </section>
+
+  <section class="posts" id="content">
+    <div class="post-group format-vertical feature-first">
+      @while(have_posts()) @php the_post() @endphp
+
+        @include('partials.post-item', [
+          'post_classes' => 'col-md-4 col-lg-3',
+          'format' => '',
+          'showExcerpt' => false,
+        ])
+      @endwhile
+    </div>
+    <div class="page-number">
+      @php echo paginate_links() @endphp
+    </div>
+  </section>
+
+@endsection
