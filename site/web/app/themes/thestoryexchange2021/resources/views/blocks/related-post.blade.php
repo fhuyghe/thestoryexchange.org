7060d65c4cfc94a9fb7ba3ddcee2a89bf79add91
Francois Huyghe <francois@13milliseconds.com>
2021-05-12 10:20:35 -0400

    related post

--- site/web/app/themes/thestoryexchange2021/resources/views/blocks/related-post.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/blocks/related-post.blade.php
@@ -18,16 +18,33 @@ $format = null;
 $showExcerpt = true;
 @endphp
 
-@if($block['related_post'])
-  <div id="{{ $block['id'] }}" class="wp-block {{ $block['classes'] }}">
+<div id="{{ $block['id'] }}" class="wp-block {{ $block['classes'] }}">
+  @if($block['related_post'])
     <div class="section-title">
-      <h3>Related</h3>
+      <h4>Related</h4>
     </div>
     @php 
       $post = $block['related_post'];
       setup_postdata($post) 
     @endphp
-    @include('partials.post-item')
+
+    <div class="post-wrap">
+      @if ( has_post_thumbnail() )
+        <div class="thumbnail">
+          <a href="{!! the_permalink() !!}" class="post-image-link">
+            {!! the_post_thumbnail('thumbnail') !!}
+          </a>
+        </div>
+      @endif
+      <div class="entry-text">
+          <h3 class="post-title">
+            <a href="{!! the_permalink() !!}">
+              {!! the_title() !!}
+            </a>
+          </h3>
+      </div>
+    </div>
+
     @php wp_reset_postdata() @endphp
-  </div>
-@endif
+    @endif
+</div>
