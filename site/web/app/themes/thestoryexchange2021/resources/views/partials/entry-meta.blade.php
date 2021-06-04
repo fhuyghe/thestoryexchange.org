a263901a26d98548508d77f4507c813bf4fef35b
Francois Huyghe <francois@13milliseconds.com>
2021-06-04 11:32:31 -0400

    Hide the author on demand

--- site/web/app/themes/thestoryexchange2021/resources/views/partials/entry-meta.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/partials/entry-meta.blade.php
@@ -1,4 +1,6 @@
 <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+@php $hideTheAuthor = get_field('hide_the_author') @endphp
+@if(!$hideTheAuthor)
 <div class="author-meta">
     <div class="author_thumbnail">
       @if(function_exists('userphoto_the_author_thumbnail'))
@@ -8,3 +10,4 @@
     {!! _e( 'By ', 'continuum' ) !!}
     {!! the_author_link() !!}
 </div>
+@endif
