f5f607379d1d722d28697c6d7987594c4d1211d8
Francois Huyghe <francois@13milliseconds.com>
2021-12-22 12:32:01 -0500

    Featured Posts section design

--- site/web/app/themes/thestoryexchange2021/resources/views/blocks/featured-posts.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/blocks/featured-posts.blade.php
@@ -26,27 +26,27 @@ $showExcerpt = true;
   
   @if($block['posts'])
     <div class="row">
-    @if($block['style'] == 1)
-    {{-- Style 3 --}}
-    <div class="col-md-4 left">
-      @php $post = $block['posts'][0] @endphp
-      @php setup_postdata($post) @endphp
-      @include('partials.post-item')
-      @php wp_reset_postdata() @endphp
-    </div>
-    <div class="col-md-8 right">
-      <div class="post-group format-horizontal">
-    @foreach ($block['posts'] as $post) @php setup_postdata($post) @endphp
-      @if($loop->iteration > 1)
-        <div class="col-md-6">
+      @if($block['style'] == 1)
+      {{-- Style 3 --}}
+      <div class="col-md-4 left ">
+        @php $post = $block['posts'][0] @endphp
+        @php setup_postdata($post) @endphp
         @include('partials.post-item')
-        </div>
         @php wp_reset_postdata() @endphp
-      @endif
-    @endforeach
-    </div>
-    </div>
-  
+      </div>
+
+      <div class="col-md-8 right">
+        <div class="post-group format-horizontal">
+          @foreach ($block['posts'] as $post) @php setup_postdata($post) @endphp
+            @if($loop->iteration > 1)
+              <div class="col-6">
+                @include('partials.post-item')
+              </div>
+              @php wp_reset_postdata() @endphp
+            @endif
+          @endforeach
+        </div>
+      </div>
 
       @else
       {{-- Default --}}
