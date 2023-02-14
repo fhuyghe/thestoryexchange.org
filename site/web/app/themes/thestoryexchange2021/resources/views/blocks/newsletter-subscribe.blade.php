17ff4e8c2478dc89f8c6ff28daf2c625f3d33f57
Francois Huyghe <francois@13milliseconds.com>
2023-02-14 16:06:24 -0500

    reformat newsletter section

--- site/web/app/themes/thestoryexchange2021/resources/views/blocks/newsletter-subscribe.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/blocks/newsletter-subscribe.blade.php
@@ -14,7 +14,14 @@
 <div id="{{ $block['id'] }}" class="wp-block {{ $block['classes'] }}">
     <h2>{{ $block['title'] }}</h2>
     {!! $block['text'] !!}
-     <!--JB Tracker--> <script type="text/javascript"> var _paq = _paq || []; (function(){ if(window.apScriptInserted) return; _paq.push(['clientToken', 'P%2bsIjEMd6oQ%3d']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://prod.benchmarkemail.com/tracker.bundle.js'; s.parentNode.insertBefore(g,s); window.apScriptInserted=true;})(); </script> <!--/JB Tracker--> <!-- BEGIN: Benchmark Email Signup Form Code -->
-    <script type="text/javascript" id="lbscript1299648" src="https://lb.benchmarkemail.com//code/lbformnew.js?mFcQnoBFKMSecPfRIkLLIFgg49YEcrgGWS4PGTYbqIA73fD7Zl3axA%253D%253D"></script><noscript>Please enable JavaScript <br /></noscript>
-    <!-- END: Benchmark Email Signup Form Code -->
+     <!--JB Tracker--> 
+     <script type="text/javascript"> 
+      var _paq = _paq || []; 
+      (function(){ if(window.apScriptInserted) return; 
+      _paq.push(['clientToken', 'P%2bsIjEMd6oQ%3d']); 
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; 
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://prod.benchmarkemail.com/tracker.bundle.js'; 
+      s.parentNode.insertBefore(g,s); window.apScriptInserted=true;})();
+     </script> <!--/JB Tracker--> 
+    @include('partials/newsletter-signup-email')
 </div>
--- site/web/app/themes/thestoryexchange2021/resources/views/partials/newsletter-signup-email.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/partials/newsletter-signup-email.blade.php
@@ -1,19 +1,3 @@
-<!-- Begin Mailchimp Signup Form -->
-{{-- <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
-<div id="mc_embed_signup">
-<form action="https://thestoryexchange.us2.list-manage.com/subscribe/post?u=b446574c4c4c1d75475aa8b75&amp;id=00e4d55029" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b446574c4c4c1d75475aa8b75_00e4d55029" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div> --}}
-
-<!--End mc_embed_signup-->
-
 <div id="embed_signup">
  <!--JB Tracker--> <script type="text/javascript"> var _paq = _paq || []; (function(){ if(window.apScriptInserted) return; _paq.push(['clientToken', 'P%2bsIjEMd6oQ%3d']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://prod.benchmarkemail.com/tracker.bundle.js'; s.parentNode.insertBefore(g,s); window.apScriptInserted=true;})(); </script> <!--/JB Tracker--> <!-- BEGIN: Benchmark Email Signup Form Code -->
  <script type="text/javascript" id="lbscript1299648" src="https://lb.benchmarkemail.com//code/lbformnew.js?mFcQnoBFKMSecPfRIkLLIFgg49YEcrgGWS4PGTYbqIA73fD7Zl3axA%253D%253D"></script><noscript>Please enable JavaScript <br /></noscript>
