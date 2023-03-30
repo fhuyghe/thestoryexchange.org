198ccbfa58c0fb54cc663d8dd419c1f8cfc9aee6
Francois Huyghe <francois@13milliseconds.com>
2023-03-30 16:35:31 -0400

    Add Smashh balloon

--- site/web/app/themes/thestoryexchange2021/resources/views/blocks/newsletter-subscribe.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/blocks/newsletter-subscribe.blade.php
@@ -14,14 +14,5 @@
 <div id="{{ $block['id'] }}" class="wp-block {{ $block['classes'] }}">
     <h2>{{ $block['title'] }}</h2>
     {!! $block['text'] !!}
-     <!--JB Tracker--> 
-     <script type="text/javascript"> 
-      var _paq = _paq || []; 
-      (function(){ if(window.apScriptInserted) return; 
-      _paq.push(['clientToken', 'P%2bsIjEMd6oQ%3d']); 
-      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; 
-      g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://prod.benchmarkemail.com/tracker.bundle.js'; 
-      s.parentNode.insertBefore(g,s); window.apScriptInserted=true;})();
-     </script> <!--/JB Tracker--> 
     @include('partials/newsletter-signup-email')
 </div>
