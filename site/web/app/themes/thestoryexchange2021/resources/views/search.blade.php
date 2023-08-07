9c54533d67643628b5333e3d8c352674a64963fd
Francois Huyghe <francois@13milliseconds.com>
2023-08-07 11:06:30 -0400

    Fix search page stying

--- site/web/app/themes/thestoryexchange2021/resources/views/search.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/search.blade.php
@@ -3,18 +3,20 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+  <div class="container">
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, no results were found.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
+
+    <div class="post-group format-horizontal">
+      @while(have_posts()) @php the_post() @endphp
+        @include('partials.content-search')
+      @endwhile
     </div>
-    {!! get_search_form(false) !!}
-  @endif
 
-  <div class="post-group format-horizontal">
-    @while(have_posts()) @php the_post() @endphp
-      @include('partials.content-search')
-    @endwhile
+    {!! get_the_posts_navigation() !!}
   </div>
-
-  {!! get_the_posts_navigation() !!}
 @endsection
