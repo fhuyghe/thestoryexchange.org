bb496624a4eead414e69a1d878ba46213d873f9d
Francois Huyghe <francois@13milliseconds.com>
2023-08-17 11:23:21 -0400

    sharon edits

--- site/web/app/themes/thestoryexchange2021/resources/views/blocks/timeline-cta.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/blocks/timeline-cta.blade.php
@@ -16,7 +16,7 @@
 <div id="{{ $block['id'] }}" class="wp-block {{ $block['classes'] }}">
     <div class="timeline-cta-wrap">
         <div class="timeline-cta-text">
-        <h1>{{ get_field('title') }}</h1>
+        <h2>{{ get_field('title') }}</h2>
         <p>{{ get_field('subtitle') }}</p>
         <a class="button red" href="{{ the_field('link') }}">{{ the_field('button_text') }}</a>
         </div>
