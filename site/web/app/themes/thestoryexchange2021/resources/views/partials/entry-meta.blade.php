95b8f299ba454a960b4d4e9ad95f02d6502dddb2
Francois Huyghe <francois@13milliseconds.com>
2023-11-30 22:35:48 -0500

    smaller author thumbnail

--- site/web/app/themes/thestoryexchange2021/resources/views/partials/entry-meta.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/partials/entry-meta.blade.php
@@ -3,7 +3,10 @@
 @if(!$hideTheAuthor)
 <div class="author-meta">
     <div class="author_thumbnail">
-      @if(function_exists('userphoto_the_author_photo'))
+      @php $user_photo = get_field('user_photo', 'user_' . get_the_author_meta('ID')) @endphp
+      @if(!empty( $user_photo))
+        <img src="{!! esc_url($user_photo['sizes']['thumbnail']) !!}" alt="{{ esc_attr($user_photo['alt']) }}" />
+      @elseif(function_exists('userphoto_the_author_photo'))
         {!! userphoto_the_author_photo($post->post_author) !!}
       @endif
     </div>
