b63e786c4441b35bb8cb7331f0124bb622a1528c
Francois Huyghe <francois@13milliseconds.com>
2023-11-30 22:27:32 -0500

    update user photo tool

--- site/web/app/themes/thestoryexchange2021/resources/views/author.blade.php
+++ site/web/app/themes/thestoryexchange2021/resources/views/author.blade.php
@@ -17,7 +17,10 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
 
   <section class="author-header">
         <div class="thumbnail">
-          @if(function_exists('userphoto_the_author_photo'))
+          @php $user_photo = get_field('user_photo', 'user_' . get_the_author_meta('ID')) @endphp
+          @if(!empty( $user_photo))
+            <img src="{!! esc_url($user_photo['sizes']['medium']) !!}" alt="{{ esc_attr($user_photo['alt']) }}" />
+          @elseif(function_exists('userphoto_the_author_photo'))
             {!! userphoto_the_author_photo() !!}
           @endif
         </div>
